cb51904e4645f0b414ed491c194b8c462817e03d
Tatamo <nsorder@gmail.com>
2017-09-22 01:27:31 +0900

    test of MatrixGraph

---
+++ __tests__/graph.ts
@@ -0,0 +1,55 @@
+import {MatrixGraph} from "../lib/graph";
+
+const sample_input = `6
+0 1 0 0 1 0
+1 0 1 0 1 0
+0 1 0 1 0 0
+0 0 1 0 1 1
+1 1 0 1 0 0
+0 0 0 1 0 0
+`.split("\n");
+
+const sample_matrix = [
+	[0, 1, 0, 0, 1, 0],
+	[1, 0, 1, 0, 1, 0],
+	[0, 1, 0, 1, 0, 0],
+	[0, 0, 1, 0, 1, 1],
+	[1, 1, 0, 1, 0, 0],
+	[0, 0, 0, 1, 0, 0]
+];
+
+const sample_matrix_inf = [
+	[0, 1, Infinity, Infinity, 1, Infinity],
+	[1, 0, 1, Infinity, 1, Infinity],
+	[Infinity, 1, 0, 1, Infinity, Infinity],
+	[Infinity, Infinity, 1, 0, 1, 1],
+	[1, 1, Infinity, 1, 0, Infinity],
+	[Infinity, Infinity, Infinity, 1, Infinity, 0]
+];
+
+const sample_wf_result = [
+	[0, 1, 2, 2, 1, 3],
+	[1, 0, 1, 2, 1, 3],
+	[2, 1, 0, 1, 2, 2],
+	[2, 2, 1, 0, 1, 1],
+	[1, 1, 2, 1, 0, 2],
+	[3, 3, 2, 1, 2, 0]
+];
+
+describe("Matrix Graph test", () => {
+	const graph = new MatrixGraph(sample_input, 6, 1, {zero_as_no_edge: true});
+	test("string input", () => {
+		expect(new MatrixGraph(sample_input, 6, 1).getArray()).toEqual(sample_matrix);
+		expect(new MatrixGraph(sample_input, 6, 1, {zero_as_no_edge: true}).getArray()).toEqual(sample_matrix_inf);
+	});
+	test("array input", () => {
+		expect(new MatrixGraph(sample_matrix).getArray()).toEqual(sample_matrix);
+		expect(new MatrixGraph(sample_matrix, {zero_as_no_edge: true}).getArray()).toEqual(sample_matrix_inf);
+	});
+	test("graph size", () => {
+		expect(graph.size).toBe(6);
+	});
+	test("Warshall-Floyd", () => {
+		expect(graph.warshallFloyd()).toEqual(sample_wf_result);
+	});
+});
